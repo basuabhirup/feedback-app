8b247b3780f0c227887043bbab4c93e964a75d93
basuabhirup <basu.abhirup@yahoo.com>
2022-02-26 16:24:32 +0530

    Add Fade Animation Effect with framer-motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,26 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({ feedbackArray, handleDelete }) {
 	return (
 		<div className='feedback-list'>
 			{feedbackArray && feedbackArray.length > 0 ? (
-				feedbackArray.map(feedback => (
-					<FeedbackItem
-						key={`feedback-id-${feedback.id}`}
-						feedback={feedback}
-						handleDelete={handleDelete}
-					/>
-				))
+				<AnimatePresence>
+					{feedbackArray.map(feedback => (
+						<motion.div
+							key={feedback.id}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+						>
+							<FeedbackItem
+								key={`feedback-id-${feedback.id}`}
+								feedback={feedback}
+								handleDelete={handleDelete}
+							/>
+						</motion.div>
+					))}
+				</AnimatePresence>
 			) : (
 				<p>No Feedback Yet</p>
 			)}
