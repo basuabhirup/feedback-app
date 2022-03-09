9e06023cd191ead70006ca4f0bbf73f5134c36bb
basuabhirup <basu.abhirup@yahoo.com>
2022-03-09 20:01:50 +0530

    Create Route to About page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import { Fragment, useState } from 'react'
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
 import { v4 as uuidv4 } from 'uuid'
 
 function App() {
 	const [feedbackArray, setFeedbackArray] = useState(FeedbackData)
 
-	const addFeedback = (newFeedback) => {
+	const addFeedback = newFeedback => {
 		newFeedback.id = uuidv4()
 		setFeedbackArray([newFeedback, ...feedbackArray])
 	}
 
-	const deleteFeedback = (id) => {
+	const deleteFeedback = id => {
 		if (window.confirm('Are you sure you want to delete?')) {
 			setFeedbackArray(feedbackArray.filter(feedback => feedback.id !== id))
 		}
 	}
-	
+
 	return (
-		<Fragment>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedbackArray={feedbackArray} />
-				<FeedbackList
-					feedbackArray={feedbackArray}
-					handleDelete={deleteFeedback}
-				/>
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedbackArray={feedbackArray} />
+								<FeedbackList
+									feedbackArray={feedbackArray}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					/>
+					<Route exact path='/about' element={<AboutPage />} />
+				</Routes>
 			</div>
-		</Fragment>
+		</Router>
 	)
 }
 
